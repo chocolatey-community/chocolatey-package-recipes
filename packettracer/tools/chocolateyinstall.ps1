c6adca41172d091ffff9a49cbfba37198f14e13b
Graham Smith <gsmitheidw@gmail.com>
2018-09-30 15:03:33 +0100

    Cisco Packet Tracer
    Recipe for 7.1.1

---
+++ packettracer/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = "PATH_TO_UNC_PATH_OR_NUGET_SERVER" 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  file          = $url
+
+  softwareName  = 'packettracer*' 
+
+
+  checksum      = 'E41A5664241B6EF4341AF57BD8867D4787D618841ECCC8D98854EC7341B83263'
+  checksumType  = 'sha256' 
+ 
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes= @(0, 3010, 1641)
+  
+}
+
+Install-ChocolateyInstallPackage @packageArgs 
+
---
+++ packettracer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿$cptapp = "$env:ProgramFiles\Cisco Packet Tracer 7.1.1\unins000.exe" 
+$cptarguments ="/SILENT"
+start-process $cptapp $cptarguments
